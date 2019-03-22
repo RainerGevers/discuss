530b8ec4844dd60ce8d05bb8515193aada68cb33
Rainer <rainer@fastcomm.com>
2019-03-22 11:24:39 +0200

    Allowed login and logout

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -5,7 +5,6 @@ defmodule Discuss.TopicController do
 
   def index(conn, _params) do
     topics = Repo.all(Topic)
-
     render conn, "index.html", topics: topics
   end
 
@@ -38,7 +37,7 @@ defmodule Discuss.TopicController do
     old_topic = Repo.get(Topic, topic_id)
     changeset = Topic.changeset(old_topic, topic)
     case Repo.update(changeset) do
-      {:ok, _topic} -> 
+      {:ok, _topic} ->
         conn
         |> put_flash(:info, "Topic Updated")
         |> redirect(to: topic_path(conn, :index))
